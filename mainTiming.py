420560eace9f3346860beac8c5558d1c03c5cd44
barinab <babak.bayanian@gmail.com>
2023-05-26 10:25:34 +0330

    firstly

--- mainTiming.py
+++ mainTiming.py
@@ -1,4 +1,28 @@
 #schedule for timing
+import TEST1
+def timing_schu(Fun,FTime):
 
-import datetime
+    import time
+    c=Fun
+#saving time when run APP
+    NOW=time.time()
+# check time
+    perTime=NOW+FTime
+    
+    secTime=time.time()
+    while True: 
+        secTime=time.time()
 
+        
+        if secTime>perTime:
+            print(secTime)
+            print(perTime)
+            c
+            NOW=time.time()
+            break
+        else:
+            pass
+     
+b=TEST1.a()
+    
+timing_schu(b,20)
